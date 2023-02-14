08f7a951c915ec0d1ba16a9df9bf73806ee3a63b
jjohnsen123 <johnsenjeppe@gmail.com>
2023-02-14 10:54:09 +0100

    DagligFast commit DagligSkaev commit

--- ordination/DagligFast.java
+++ ordination/DagligFast.java
@@ -1,8 +1,9 @@
 package ordination;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
 
-public class DagligFast {
+public class DagligFast extends Ordination {
 
     private final Dosis[] dosis = new Dosis[4];
     private double morgenAntal;
@@ -14,12 +15,37 @@ public class DagligFast {
     private LocalTime aften = LocalTime.of(18,0,0);
     private LocalTime nat = LocalTime.of(22,0,0);
 
+    public DagligFast(LocalDate startDen, LocalDate slutDen, ordination.Patient patient,
+                      double morgenAntal, double middagAntal, double aftenAntal, double natAntal) {
+        super(startDen, slutDen, patient);
+        if (morgenAntal > 0) {
+            dosis[0] = new Dosis(morgen, morgenAntal);
+        }
+        if (middagAntal > 0) {
+            dosis[1] = new Dosis(middag, middagAntal);
+        }
+         if (aftenAntal > 0) {
+             dosis[2] = new Dosis(aften, aftenAntal);
+         }
+         if (natAntal > 0) {
+             dosis[3] = new Dosis(nat, natAntal);
+         }
+    }
 
-    public DagligFast(double morgenAntal, double middagAntal, double aftenAntal, double natAntal) {
-        dosis[0] = new Dosis(morgen, morgenAntal);
-        dosis[1] = new Dosis(middag, middagAntal);
-        dosis[2] = new Dosis(aften, aftenAntal);
-        dosis[3] = new Dosis(nat, natAntal);
+
+    @Override
+    public double samletDosis() {
+        return 0;
+    }
+
+    @Override
+    public double doegnDosis() {
+        return 0;
+    }
+
+    @Override
+    public String getType() {
+        return null;
     }
 
     // TODO
--- ordination/DagligSkaev.java
+++ ordination/DagligSkaev.java
@@ -1,12 +1,18 @@
 package ordination;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 
-public class DagligSkaev{
+public class DagligSkaev extends Ordination {
+
     private LocalTime tid;
     private double antal;
     private final ArrayList<Dosis> doser = new ArrayList<>();
+
+    public DagligSkaev(LocalDate startDen, LocalDate slutDen, ordination.Patient patient) {
+        super(startDen, slutDen, patient);
+    }
 
     public void opretDosis(LocalTime tid, double antal) {
         Dosis dosis = new Dosis(tid, antal);
@@ -14,5 +20,19 @@ public class DagligSkaev{
     }
 
 
+    @Override
+    public double samletDosis() {
 
+        return 0;
+    }
+
+    @Override
+    public double doegnDosis() {
+        return 0;
+    }
+
+    @Override
+    public String getType() {
+        return null;
+    }
 }
