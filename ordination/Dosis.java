9b428a794d36075fe4e1d109939626ccfdd34c55
jjohnsen123 <johnsenjeppe@gmail.com>
2023-02-14 10:21:51 +0100

    DagligFast commit DagligSkaev commit Dosis link commit

--- ordination/Dosis.java
+++ ordination/Dosis.java
@@ -5,11 +5,23 @@ import java.time.LocalTime;
 public class Dosis {
     private LocalTime tid;
     private double antal;
+    private DagligFast dagligFast;
+    private DagligSkaev dagligSkaev;
 
     public Dosis(LocalTime tid, double antal) {
         super();
         this.tid = tid;
         this.antal = antal;
+    }
+
+    public Dosis(LocalTime tid, double antal, DagligSkaev dagligSkaev) {
+        this(tid,antal);
+        this.dagligSkaev = dagligSkaev;
+    }
+
+    public Dosis(LocalTime tid, double antal, DagligFast dagligFast) {
+        this(tid, antal);
+        this.dagligFast = dagligFast;
     }
 
     public double getAntal() {
