5dba8fbed13e4da0ca3b8780e15cad277b16023a
JohnLe <johnle@hotmail.dk>
2023-02-16 09:23:19 +0100

    test

---
+++ test/ordination/DagligFastTest.java
@@ -0,0 +1,30 @@
+package ordination;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DagligFastTest {
+    @org.junit.jupiter.api.Test
+    void TC1_doegnDosis() {
+        //Arrange
+        //Act
+        //Assert
+    }
+
+    @Test
+    void TC2_doegnDosis() {
+        //Arrange
+        //Act
+        //Assert
+    }
+
+    @Test
+    void TC3_doegnDosis() {
+        //Arrange
+        //Act
+        //Assert
+    }
+    //------------------------------------------------------------------------------------------------------------------
+
+}
